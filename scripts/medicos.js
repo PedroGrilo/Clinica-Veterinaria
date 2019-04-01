45e7e261f6b58823dee39d482f63db3daec6a7ba
PedroGrilo <xpereiragrilo@gmail.com>
2019-04-01 23:08:31 +0100

    so copiei da lista de consultas para o medicos

--- scripts/medicos.js
+++ scripts/medicos.js
@@ -1,9 +1,12 @@
 /**
  * Classe Medico
  */
+
 /**
  * @constructs Medico
  */
+
+
 
 function Medico(id, nome, titulo, genero, email, especialidade, foto) {
     this.id = id;
@@ -18,11 +21,93 @@ function Medico(id, nome, titulo, genero, email, especialidade, foto) {
 /*
  * Classe Lista de medicos
  */
+
 /**
  * @constructs ListaMedicos
  */
 
+
+
+
 function ListaMedicos() {
-    this.medicos = [];
+    if (localStorage['ListaMedicos']) {
+        var retrievedObject = JSON.parse(localStorage.getItem('ListaMedicos'));
+        this.medicos = retrievedObject;
+    } else {
+        this.medicos = [];
+    }
+
 }
 
+ListaMedicos.prototype.saveMedicos = function () { //guardar no localStorage
+    localStorage['ListaMedicos'] = JSON.stringify(this.medicos);
+}
+
+ListaMedicos.prototype.acrescentarMedico = function (medico) {
+    this.medicos.push(medico);
+    this.saveMedicos();
+}
+
+ListaMedicos.prototype.acrescentarMedicos = function (medico) {
+    medico = Array.prototype.slice.call(arguments); //Transformar o "arguments" num array par poder usar o forEach
+    medico.forEach(function (currentValue, index, array) {
+        this.acrescentarMedico(currentValue);
+    }, this);
+    return this;
+
+};
+
+ListaMedicos.prototype.listarMedicos = function () {
+
+    if (this.medicos.length === 0) {
+        return "<h4>Não existem consultas na base de dados!</h4>";
+
+    } else {
+        var resultado = `<h2>Lista de Medicos</h2><table><tr><th>Medico</th><th>Titulo</th><th>Genero</th><th>Email</th><th>Especialidade</th></tr>`;
+
+        this.medicos.forEach(function (currentValue, index, array) {
+            resultado += "<tr><td> " + currentValue.nome + "</td><td> " + currentValue.titulo + "</td><td>" + currentValue.genero + "</td><td>" + currentValue.email + "</td><td>" + currentValue.especialidade + "</td></tr>";
+            today = true;
+
+        });
+        resultado += "</table>";
+
+        return resultado;
+    }
+
+};
+
+ListaMedicos.prototype.getMedicosLocal = function () { //guardar as consultas no array de consultas
+    if (localStorage['ListaMedicos']) {
+        this.medicos = JSON.parse(localStorage['ListaMedicos']);
+    }
+}
+
+ListaMedicos.apresentar = function (medico) {
+    medico = medico || new ListaMedicos().acrescentarMedicos();
+    medico.getMedicosLocal();
+    document.getElementById("listaMedicos").innerHTML = medico.listarMedicos();
+    medico.saveConsultas();
+
+};
+
+
+ListaMedicos.acrescentar = function (medico) {
+    var nome = document.getElementById("nome").value;
+    var titulo = document.getElementById("titulo").value;
+    var genero = document.getElementById("genero").value;
+    var email = document.getElementById("email").value;
+    var especialidade = document.getElementById("especialidade").value;
+    var foto = document.getElementById("foto").value;
+
+
+    if (nome != "" && titulo != "" && genero != "" && email != "" && especialidade != "" && foto != "") {
+        medico = new ListaMedicos().acrescentarMedicos();
+        medico.acrescentarMedico(new Medico());
+        ListaMedicos.apresentar(medico);
+    } else {
+        alert("Obrigatório preencher todos os campos!");
+    }
+
+
+};
