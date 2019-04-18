aae70b7885469022583f8cf2541ffe509d568961
PedroGrilo <xpereiragrilo@gmail.com>
2019-04-18 16:51:24 +0100

    comentários e correçao de bugs

--- scripts/consultas.js
+++ scripts/consultas.js
@@ -79,26 +79,38 @@ function createMarcacoes(medicoSelected) {
     idHeader.textContent = "Consultas do médico " + medico + " nos próximos 7 dias";
 
 
-    for (var i = 0; i < 7; i++) {
-        var tr = document.createElement("TR");
-        var date = new Date();
-        var td = document.createElement("TD");
-        var text = document.createTextNode(date.addDays(i));
+    for (let i = 0; i < 7; i++) {
+        let tr = document.createElement("TR");
+        let date = new Date();
+        let td = document.createElement("TD");
+        let text = document.createTextNode(date.addDays(i));
 
-        tr.setAttribute("id","myTR"+i);
+        tr.setAttribute("id", "myTR" + i);
         tBody.appendChild(tr);
         td.appendChild(text);
-        document.getElementById("myTR"+i).appendChild(td);
+        document.getElementById("myTR" + i).appendChild(td);
 
-        var listaConsultasLocal = JSON.parse(localStorage['ListaConsultas']);
+        let listaConsultasLocal = JSON.parse(localStorage['ListaConsultas']);
 
-        for (var h = 0; h < listaConsultasLocal.length; h++) {
-            if ((listaConsultasLocal[h].medico == medico) && (listaConsultasLocal[h].diaDaConsulta == text.textContent)) {
-                tr.setAttribute("class", "checked");
-                break;
+        let counter = 0, hoje = 0, horasRestantes = 0;
+
+        if (data.getHoras() < 14) // verificar quantos espaços existem conforme a hora atual
+            horasRestantes = 18 - data.getHoras() - 1; // o menos 1, é por causa da hora de almoço
+        else
+            horasRestantes = 18 - data.getHoras();
+
+        for (let h = 0; h < listaConsultasLocal.length; h++) {
+            if (listaConsultasLocal[h].diaDaConsulta == data.getDataAtual())
+                hoje++;
+            if (listaConsultasLocal[h].diaDaConsulta == text.textContent) {
+                counter++;
+                if ((listaConsultasLocal[h].medico == medico)) 
+                    tr.setAttribute("class", "checked");
             }
         }
-
+        if (counter >= 8 || hoje > horasRestantes) 
+            tr.setAttribute("class", "notAvailableDate");
+        
     }
 }
 
@@ -109,7 +121,7 @@ function createMarcacoes(medicoSelected) {
 function listHours(data, medico) {
 
     var listaConsultasLocal = JSON.parse(localStorage['ListaConsultas']);
-    
+
     document.getElementById("theadMarc").hidden = false;
 
     var tBody = document.getElementById("tbodySemana");
@@ -125,23 +137,23 @@ function listHours(data, medico) {
         var td = document.createElement("TD");
         var text;
 
-        tr.setAttribute("id",i);
+        tr.setAttribute("id", i);
         tBody.appendChild(tr);
         document.getElementById(i).appendChild(td);
-        
+
         for (var h = 0; h < listaConsultasLocal.length; h++) {
-            if ((listaConsultasLocal[h].medico == medico) && (listaConsultasLocal[h].diaDaConsulta == data.value) && (listaConsultasLocal[h].hora == i)) {
+            if ((listaConsultasLocal[h].medico == medico) && (listaConsultasLocal[h].diaDaConsulta == data.value) && (listaConsultasLocal[h].hora == i)) { //verificar disponibilidade da consulta
                 tr.setAttribute("class", "notAvailable");
                 createSpans(i + ":00", td, "notAvailable");
                 text = document.createTextNode(" - Indisponível - " + listaConsultasLocal[h].nomeDoAnimal);
-            } 
+            }
         }
 
         if (i == 14) { //hora de almoco
             tr.setAttribute("class", "notAvailable");
             createSpans(i + ":00", td, "notAvailable");
             text = document.createTextNode(" - Hora de almoço");
-        } else if(!tr.getAttribute("class")){
+        } else if (!tr.getAttribute("class")) { //hora normal
             createSpans(i + ":00", td, "hours" + i);
             text = document.createTextNode(" - Clique para adicionar");
             tr.setAttribute("onclick", "checkHour(this)");
@@ -162,12 +174,12 @@ function checkHour(tr) {
         var table = document.getElementById('marcacoesTable');
         var numberChilds = table.rows.length;
 
-        for (var i = 1; i < numberChilds; i++) {
+        for (var i = 1; i < numberChilds; i++) {// se algum ja tiver selecionado, tirar a classe checked a todos 
             if (table.rows[i].getAttribute("class") == "checked")
                 table.rows[i].removeAttribute("class");
         }
 
-        tr.setAttribute("class", "checked");
+        tr.setAttribute("class", "checked"); // e colocar a class aquele que foi selecionado
         tr.text = "teste";
     }
 }
@@ -268,7 +280,7 @@ function listarMedicos(opcao) {
 }
 
 /**
- * Quardar as consultas no localStoraage
+ * Quardar as consultas no localStorage
  */
 ListaConsulta.prototype.saveConsultas = function () {
     localStorage['ListaConsultas'] = JSON.stringify(this.consultas);
@@ -376,14 +388,14 @@ function checkOption(getSelected) {
 ListaConsulta.prototype.listarConsultas = function () {
     var today = false;
     console.log(this.consultas);
-    if (this.consultas.length === 0) {
+    if (this.consultas.length === 0) { //quando nao ha dados na localStorage
         return "<h4>Não existem consultas na base de dados!</h4>";
 
     } else {
         var resultado = `<table class='table'><thead class="thead-dark"><tr><th>Hora</th><th>Medico</th><th>Nome do Animal</th><th>Tipo de Consulta</th><th>Efetivada</th><th>Paga</th><th>Remover</th><th>Efetivar/Pagar</th></tr></thead>`;
 
-        this.consultas.forEach(function (currentValue, index, array) {
-            if (currentValue.diaDaConsulta === data.getDataAtual()) { // verificar se as consultas é para o dia atual
+        this.consultas.forEach(function (currentValue, index, array) { //percorrer o array para criar a tabela
+            if (currentValue.diaDaConsulta === data.getDataAtual()) { // verificar se as consultas são para o dia atual
                 var remove = `<td><a onclick="ListaConsulta.removerConsulta(` + currentValue.id + `)" class='far fa-times-circle'></a></td>`;
                 resultado += "<tr><td> " + currentValue.hora + ":00</td><td> " + currentValue.medico + "</td><td> " + currentValue.nomeDoAnimal + "</td><td>" +
                     currentValue.tipoDeConsulta + "</td><td>" + currentValue.efetivada + "</td><td>" + currentValue.paga + "</td>" +
@@ -395,7 +407,7 @@ ListaConsulta.prototype.listarConsultas = function () {
         if (today)
             resultado += "</table>";
         else
-            resultado = "<h4>Não existem consultas para hoje! :(</h4>";
+            resultado = "<h4>Não existem consultas para hoje! :(</h4>"; //Quando nao ha consultas para o dia de hoje na localStorage
 
         return resultado;
     }
@@ -431,7 +443,7 @@ function isNull(campo) {
 /**
  * Mnada um alert e dá focus ao campo, quando dá erro
  */
-function alertAndFocus(campo,msg,classError) {
+function alertAndFocus(campo, msg, classError) {
     alert(msg);
     campo.focus();
     campo.setAttribute("class", classError);
@@ -441,24 +453,23 @@ function alertAndFocus(campo,msg,classError) {
 /**
  * Verificações ao adicionar consulta
  */
-function checkConsulta(medico, nomeAnimal, tipoConsulta, dataInput,hora) {
-
+function checkConsulta(medico, nomeAnimal, tipoConsulta, dataInput, hora) {
     if (isNull(dataInput.value))
-        alertAndFocus(dataInput, "Data inválida","input-error form-control");
+        alertAndFocus(dataInput, "Data inválida", "input-error form-control");
     else if (dataInput.value < data.getDataAtual())
-        alertAndFocus(dataInput, "A data têm que ser igual ou superior à do dia de hoje","input-error form-control");
-    else if(isNull(hora))
+        alertAndFocus(dataInput, "Data Indisponível", "input-error form-control");
+    else if (isNull(hora))
         alert("Hora inválida");
-    else if(hora.id < data.getHoras() && dataInput.value == data.getDataAtual())  //comentar para testes
-        alertAndFocus(hora,"A Hora de consulta têm de ser maior que a hora atual!");
+    else if (hora.id <= data.getHoras() && dataInput.value == data.getDataAtual())  //comentar para testes
+        alertAndFocus(hora, "Hora indisponível!");
     else if (isNull(nomeAnimal.value))
-        alertAndFocus(nomeAnimal, "O campo 'Nome do Animal' é um obrigatório!","input-error form-control");
+        alertAndFocus(nomeAnimal, "O campo 'Nome do Animal' é um obrigatório!", "input-error form-control");
     else if (isNull(tipoConsulta.value))
-        alertAndFocus(tipoConsulta, "O campo 'Tipo de Consulta' é obrigatório!","input-error form-control");
+        alertAndFocus(tipoConsulta, "O campo 'Tipo de Consulta' é obrigatório!", "input-error form-control");
     else if (isNull(medico.value))
-        alertAndFocus(medico, "Obrigatório escolher um médico","input-error form-control");
+        alertAndFocus(medico, "Obrigatório escolher um médico", "input-error form-control");
     else if (medico.value == "Não existem médicos")
-        alertAndFocus("Não existem médicos para este tipo de consulta","input-error form-control");
+        alertAndFocus("Não existem médicos para este tipo de consulta", "input-error form-control");
     else
         return true;
 
@@ -469,15 +480,15 @@ function checkConsulta(medico, nomeAnimal, tipoConsulta, dataInput,hora) {
  */
 ListaConsulta.acrescentar = function (consulta) {
     try {
-        
+
         var table = document.getElementById('marcacoesTable');
         var numberChilds = table.rows.length;
         var hora;
 
         for (var i = 1; i < numberChilds; i++) {
-            if (table.rows[i].getAttribute("class") == "checked"){
-                 hora = table.rows[i];
-                 break;
+            if (table.rows[i].getAttribute("class") == "checked") {
+                hora = table.rows[i]; // ir buscar a hora selecionada ao mapa da semana
+                break;
             }
         }
 
@@ -485,8 +496,8 @@ ListaConsulta.acrescentar = function (consulta) {
         var nAnimal = document.getElementById("nomeAnimal");
         var tipoConsulta = document.getElementById("tipoConsulta");
         var medico = document.getElementById("medicos");
-  
-        if (checkConsulta(medico, nAnimal, tipoConsulta, dataInput,hora)) {
+
+        if (checkConsulta(medico, nAnimal, tipoConsulta, dataInput, hora)) {
             consulta = new ListaConsulta().acrescentarConsultas();
             consulta.acrescentarConsulta(new Consulta((ListaConsulta.getNumberOfConsultas() + 1), dataInput.value, hora.id, medico.value, nAnimal.value, tipoConsulta.value, 'Não', 'Não'));
             alert("Consulta adicionada com sucesso");
